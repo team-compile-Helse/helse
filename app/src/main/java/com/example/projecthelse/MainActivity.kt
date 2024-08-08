83d89029f37274182391973489ae113858c91c96
rlamnji <rlamnji@naver.com>
2024-08-08 17:09:33 +0900

    helse greeting screen

--- app/src/main/java/com/example/projecthelse/MainActivity.kt
+++ app/src/main/java/com/example/projecthelse/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.projecthelse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projecthelse.ui.theme.ProjectHelseTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +39,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(modifier: Modifier = Modifier) {
+    //배경
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color(0xFF73BBA3)), // 배경색
+        contentAlignment = Alignment.Center // 중앙 정렬
+    ){
+        // 이미지 추가
+        val image = painterResource(id = R.drawable.group_1)
+        Image(
+            painter=image,
+            contentDescription = null,
+            modifier = Modifier.size(300.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ProjectHelseTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
